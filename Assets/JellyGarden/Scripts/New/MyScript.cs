93f06815e6eadc06c5a9df1b3f118e9a146106f6
icynare <icynare@gmail.com>
2018-08-09 02:15:47 +0800

    [Add] 首次提交

---
+++ Assets/JellyGarden/Scripts/New/MyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+    int levelNum = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OnClickPlay()
+    {
+        LevelsMap.OnLevelSelected(levelNum);
+        this.transform.Find("HomePage").gameObject.SetActive(false);
+    }
+
+}
